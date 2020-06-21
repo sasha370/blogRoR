399c286b0624b03511cc2616e32982321a7effff
Alex <kng.sasha@mail.ru>
2020-06-21 16:53:33 +0300

    Articals Edit & Update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,29 +5,52 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
   # вторым шагом надо прописать обработчик для действия new
   def new
   end
+
+
+
+
 
   # далее создаем файл в папке views/article? который автоматом будет привязан по названия к данному контроллеру
   # данный контролер\метод нужен для того, чтобы создать новую запись используя страницу new и кнопку submit
   def create
     @article = Article.new(article_params)
     # Проверяем, если переменная создалась, то мы ее записываем в БД
-    if @article.valid?
-      @article.save
+    if @article.save
       # вместо метода Create мы перенаправляем на метод SHOW с помощью юданной команды. это позволяет избавиться от Вьюхи Create
       redirect_to @article
     else
       # Если не создалась, то просто вызвращаем на стартовую страницу
-      render action:  'new'
+      render action: 'new'
     end
   end
 
   # Метод отображает страницу с соответсвующим постом
   def show
-    @article =Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
+
+
+  # В петоде Edit отображается тоже самое что и SHOW т.к. это просто показ карточки
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+
+  # метод принимает на сервере данные из EDIT
+  def update
+    @article = Article.find(params[:id]) # ищем в БД необходимый пост
+    if @article.update(article_params) # проверяем, правильно ли все апгреднулось
+      redirect_to @article # если да, то перенаправляем на саму статью
+    else
+      render action: 'edit' # если нет, то возвращаем в форму редактирования
+    end
+  end
+
+
 
   # Создаем отдельный приватный метод, который разрешает передачу Полей из форму в нашу БД
 
