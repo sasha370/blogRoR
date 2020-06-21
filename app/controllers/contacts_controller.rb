ff856ef03e3efa9e98f313bc080ceb9ab5589a02
Alex <kng.sasha@mail.ru>
2020-06-21 12:28:40 +0300

    Display validation errors

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,16 +7,17 @@ class ContactsController < ApplicationController
   # данный метод нужен для оюработки отправленных на сервер данных  и записи их в БД
   def create
     @contact = Contact.new(contact_params)
+    # Проверяем, если переменная создалась, то мы ее записываем в БД
     if @contact.valid?
     @contact.save
     else
+      # Если не создалась, то просто вызвращаем на стартовую страницу
       render action: "new"
     end
   end
 
 
   # Создаем отдельный приватный метод, который разрешает передачу Полей из форму в нашу БД
-
   private
   def contact_params
     params.require(:contact).permit(:email, :message)
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
 
 
   # Данная строчка показывает маршрут к Контролеру Contact у которого разрешены только просмотр и создание нового ( методы NEW и CREATE )
-  resource :contacts,  only: [:new, :create]
+  # метод path_names перенаправляет стандартный new на пустую строчку, чтобы убрать из URL
+  resource :contacts,  only: [:new, :create], path_names:  {:new => ''}
 
-
-    resources :articles
+  resources :articles
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
