cf96fc20374e7a21df876b0b969137ab0d962cf9
Alex <kng.sasha@mail.ru>
2020-06-21 12:03:28 +0300

    save Contacts on submit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,7 +6,16 @@ class ContactsController < ApplicationController
 
   # данный метод нужен для оюработки отправленных на сервер данных  и записи их в БД
   def create
+    @contact = Contact.new(contact_params)
+    @contact.save
+  end
 
+
+  # Создаем отдельный приватный метод, который разрешает передачу Полей из форму в нашу БД
+
+  private
+  def contact_params
+    params.require(:contact).permit(:email, :message)
   end
 
 end
