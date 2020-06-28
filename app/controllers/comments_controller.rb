963f202ce7a42462bbdc84c2da23fa1c5cf52f36
sasha370 <kng.sasha@mail.ru>
2020-06-28 20:41:24 +0300

    end of day

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,20 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, :only => [:create]
+
   def create
     # находим нужную статью по ее ШВ
     @article = Article.find(params[:article_id])
     # в ней создаем новый комментарий методом Create< который сразу создает запись в БД.
     # на вход принимает проверенные данные из метода Comments_params
-    @article.comments.create(comment_params)
+    comment =  @article.comments.new(comment_params)
+
+    TODO
+    if user_signed_in?
+      comment.author = current_user.username
+      comment.save
+    else
+      redirect_to new_user_session_path
+    end
     # перенаправляем на страницу с данным Статьей
     redirect_to article_path(@article)
   end
