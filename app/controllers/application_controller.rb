9931356d0ba973a86e91aa7c4ab54190ab8ed0c1
sasha370 <kng.sasha@mail.ru>
2020-06-28 19:39:21 +0300

    Permit for Username Form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  # Если используется контроллер Devise? то провести проверку методом ниже
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  # разрешаем контролдлеру принимать параметр под названием Username
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    end
 end
