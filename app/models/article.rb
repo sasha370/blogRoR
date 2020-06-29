366a79d073d27abc3390ea631d0cb0d56fb8fe53
sasha370 <kng.sasha@mail.ru>
2020-06-29 18:04:35 +0300

    add Article and Comment validation  at leght

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :text, presence: true
+  validates :title, length: { maximum: 140 }
+  validates :text, length: { maximum: 4000 }
   has_many :comments
 
   # метод, который возращает название статьи. завели для тестироваия
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
+  validates :body, length: { maximum: 4000 }
   belongs_to :article
 end
