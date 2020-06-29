de446d1f652122499b9aed7ca6deb729c76b8999
sasha370 <kng.sasha@mail.ru>
2020-06-29 18:10:10 +0300

    add Comment leght test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,4 +4,5 @@ require 'rails_helper'
 # Модель Comment должна иметь только одну статью
 describe Comment do
   it {should belong_to :article}
+  it { should validate_length_of(:body).is_at_most(4000) }
 end
