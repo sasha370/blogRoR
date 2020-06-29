abc1282be335d3fb1d838e3ed3f89ff499b60fe2
sasha370 <kng.sasha@mail.ru>
2020-06-29 20:08:52 +0300

    add Contacts test

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 
 
 # Новый тест с помощью Capibara
+# посетить страницу Contacts и найти там строчку "....."
 feature "Contact creation" do
   scenario "allows acces to contacts page" do
-    visit '/contacts'
-    expect(page).to have_content 'Список всех контактов'
+    visit '/contacts/new'
+    expect(page).to have_content I18n.t('contacts.contact_us')
   end
 end
