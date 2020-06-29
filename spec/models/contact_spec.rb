b7afd2366ba7ffd1699d4688c96e7c1b9050fcdb
sasha370 <kng.sasha@mail.ru>
2020-06-29 16:43:31 +0300

    rebuild Contact spec

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 require 'rails_helper'
 
-
+# Данные тесты использую Gem shoulda/ который позволяет тестировать Модели
+# В модели Contact мы проверяем присутствует ли валидация на поля Email и Message
+#
 describe Contact do
   it  { should validate_presence_of(:email) }
   it  { should validate_presence_of(:message) }
