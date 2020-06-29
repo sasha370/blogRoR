09bbea0752ddf71458dc09607865715c3cf3e382
sasha370 <kng.sasha@mail.ru>
2020-06-29 16:48:18 +0300

    add Article spec validation

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,5 +3,12 @@ require 'rails_helper'
 
 # Модель Article должна иметь множество коммитов
 describe Article do
-  it {should have_many :comments}
+  describe "validates" do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:text) }
+  end
+
+  describe " associashion" do
+    it { should have_many :comments }
+    end
 end
