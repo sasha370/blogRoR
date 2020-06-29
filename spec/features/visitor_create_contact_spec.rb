0d141ddcc81d2540246b2d803cd45d8c93838c29
sasha370 <kng.sasha@mail.ru>
2020-06-29 20:15:45 +0300

    rebuild last comment

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -6,7 +6,13 @@ require 'rails_helper'
 # посетить страницу Contacts и найти там строчку "....."
 feature "Contact creation" do
   scenario "allows acces to contacts page" do
-    visit '/contacts/new'
+    visit '/contacts'
     expect(page).to have_content I18n.t('contacts.contact_us')
   end
+
+  
+
+
+
+
 end
